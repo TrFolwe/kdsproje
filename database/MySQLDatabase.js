d781f91c98d4835d2e9edd61bd63bb761e908e0e
TrFolwe <monitorminor28@gmail.com>
2023-12-21 22:47:53 +0300

    Grafik sayfası düzenlendi, üst kutu verileri eklendi geriye kalanlar chartjs ile verileri görüntüye dökmek.

--- database/MySQLDatabase.js
+++ database/MySQLDatabase.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const mysql = require('mysql2')
 require('dotenv/config')
 const { MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB_NAME, MYSQL_INSTANCE_NAME, DEVELOPER_MODE, DEVELOPER_MYSQL_PASSWORD } = process.env;
@@ -42,5 +43,42 @@ function getSoilTypeById(soilTypeId) {
     })
 }
 
-module.exports = { getProductById, getSoilTypeById };
+const wait = ms => new Promise(rs => setTimeout(rs, ms))
+
+function getGraphicsBoxDataAll() {
+    const sqlJson = {
+        tarla_sayisi: `SELECT COUNT(tarla.tarla_id) AS 'tarla_sayisi' FROM tarla`,
+        urun_sayisi: `SELECT COUNT(urun.urun_id) AS 'urun_sayisi' FROM urun`,
+        bugday_uretim_miktari: `SELECT (SUM(uretim_miktari.mayis)+SUM(uretim_miktari.haziran)+SUM(uretim_miktari.temmuz)+
+            SUM(uretim_miktari.agustos)+SUM(uretim_miktari.eylul)+SUM(uretim_miktari.ekim)) AS 'bugday_uretim_miktari'
+            FROM uretim_miktari
+            WHERE uretim_miktari.urun_id=1;`,
+        arpa_uretim_miktari: `SELECT (SUM(uretim_miktari.mayis)+SUM(uretim_miktari.haziran)+SUM(uretim_miktari.temmuz)+
+            SUM(uretim_miktari.agustos)+SUM(uretim_miktari.eylul)+SUM(uretim_miktari.ekim)) AS 'arpa_uretim_miktari'
+            FROM uretim_miktari
+            WHERE uretim_miktari.urun_id=2;`,
+        domates_uretim_miktari: `SELECT (SUM(uretim_miktari.mayis)+SUM(uretim_miktari.haziran)+SUM(uretim_miktari.temmuz)+
+            SUM(uretim_miktari.agustos)+SUM(uretim_miktari.eylul)+SUM(uretim_miktari.ekim)) AS 'domates_uretim_miktari'
+            FROM uretim_miktari
+            WHERE uretim_miktari.urun_id=3;`
+    }
+
+    const dataJson = {};
+
+    return new Promise(async (rs, rj) => {
+        mysqlPool.getConnection((err, connection) => {
+            if (err) return rj(err);
+            Object.keys(sqlJson).forEach(key => {
+                connection.query(sqlJson[key], (err, result) => {
+                    dataJson[key] = result[0][key];
+                })
+            })
+            connection.release();
+        })
+        await wait(300);
+        rs(dataJson);
+    })
+}
+
+module.exports = { getProductById, getSoilTypeById, getGraphicsBoxDataAll };
 module.exports.database = mysqlPool;
